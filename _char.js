03553dc33faf74f4d215a15d66b6ded46f9d4e37
andyhall <andy@fenomas.com>
2015-12-14 14:33:00 +0900

    Update templates

--- _char.js
+++ _char.js
@@ -3,13 +3,14 @@ var fs = require('fs')
 var readline = require('readline')
 var stream = require('stream')
 
-var stream = fs.createReadStream('input3.txt', { encoding: 'utf8' })
-
-stream.on('readable', function (s) {
-	var chunk
-	while (null !== (chunk = stream.read(1))) { work(chunk) }
-})
-stream.on('end', function () { finish() })
+function read(f) {
+	var stream = fs.createReadStream(f, { encoding: 'utf8' })
+	stream.on('readable', function (s) {
+		var chunk
+		while (null !== (chunk = stream.read(1))) { work(chunk) }
+	})
+	stream.on('end', function () { finish() })
+}
 
 
 /*
@@ -18,15 +19,33 @@ stream.on('end', function () { finish() })
  * 
 */
 
+var hash = {}
+var part2 = false
+read('test.txt')
+// read('input12.txt')
 
-var ct = 0
 
-function work(s) {
-	ct++
+function work(char) {
+	hash.chars = hash.chars ? hash.chars+1 : 1
+	
+}
+
+function solve() {
+	var ct = 0
+	for (var s in hash) {
+		ct++
+	}
+	return hash.chars
 }
 
 function finish() {
-	console.log('chars', ct)
+	var s = solve()
+	console.log('output', s)
+	
+	if (part2) {
+		s = solve()
+		console.log('output two', s)
+	}
 }
 
 
--- _line.js
+++ _line.js
@@ -3,13 +3,15 @@ var fs = require('fs')
 var readline = require('readline')
 var stream = require('stream')
 
-var instream = fs.createReadStream('input2.txt')
-var outstream = new stream.Stream
-outstream.readable = outstream.writable = true
-
-var rl = readline.createInterface({ input: instream, output: outstream, terminal: false });
-rl.on('line', function(line) { 	work(line) })
-rl.on('close', function() { 	finish() })
+function read(f) {
+	var instream = fs.createReadStream(f)
+	var outstream = new stream.Stream
+	outstream.readable = outstream.writable = true
+	
+	var rl = readline.createInterface({ input: instream, output: outstream, terminal: false });
+	rl.on('line', function(line) { 	work(line) })
+	rl.on('close', function() { 	finish() })
+}
 
 
 /*
@@ -18,15 +20,34 @@ rl.on('close', function() { 	finish() })
  * 
 */
 
+var hash = {}
+var part2 = false
+read('test.txt')
+// read('input14.txt')
 
-var ct = 0
 
 function work(line) {
-	ct++
+	var res = /(.)/.exec(line)
+	hash.lines = hash.lines ? hash.lines+1 : 1
+	hash.foo = res[1]
+}
+
+function solve() {
+	var ct = 0
+	for (var s in hash) {
+		ct++
+	}
+	return hash.lines
 }
 
 function finish() {
-	console.log('lines', ct)
+	var s = solve()
+	console.log('output', s)
+	
+	if (part2) {
+		s = solve()
+		console.log('output two', s)
+	}
 }
 
 
